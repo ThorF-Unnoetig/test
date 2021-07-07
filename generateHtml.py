87efa14a6970b429295b5110fcef0c7f2068b238
thfr <thfr@uni-bremen.de>
2021-07-07 14:44:16 +0200

    Fiexd script.

--- generateHtml.py
+++ generateHtml.py
@@ -4,6 +4,8 @@ with open("text.txt", "r") as f:
 print(text)
 
 html = "<h1>Hallo neue Seite</h1>\n<p>Just a test to get used to the pages and actions enviroment.</p>"
-
-with open("index.html", "a") as f:
+import os
+path = "output/"
+os.mkdir(path)
+with open(path + "index.html", "a") as f:
     f.write(html + "\n" + text + "\n")
