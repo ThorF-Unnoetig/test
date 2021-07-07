49678e4c5b845b4ba4c13baf465170da4bd25394
thfr <thfr@uni-bremen.de>
2021-07-07 12:59:54 +0200

    Test

--- generateHtml.py
+++ generateHtml.py
@@ -3,5 +3,7 @@ with open("text.txt", "r") as f:
 
 print(text)
 
+html = "<h1>Hallo neue Seite</h1>\n<p>Just a test to get used to the pages and actions enviroment.</p>"
+
 with open("index.html", "a") as f:
-    f.write(text + "\n")
+    f.write(html + "\n" + text + "\n")
